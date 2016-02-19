1f6bb20d3018e9c0b8c983001b5b03a58527bbaf
Olivia Ifrim <olly2204@yahoo.com>
2016-02-19 22:22:13 +0100

    Adding placeholder cards.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -29,4 +29,23 @@ controllers.controller('MyCtrl2', function ($scope) {
 
 // HomeController
 controllers.controller('HomeController', function ($scope) {
+    
+    var books = [];
+    
+    // Generate sample book
+    var sampleBook = {};
+    sampleBook.title = "Sample book";
+    sampleBook.description = "I am so interesting you will want to read me.";
+    sampleBook.author = "John Smith";
+    sampleBook.thumb = "http://placehold.it/150x200";
+    
+    // Push 10 books
+    var numberOfBooks = 10;
+    for (var bookIdx = 0; bookIdx < numberOfBooks; bookIdx++) {
+        books.push(sampleBook);
+    }
+    
+    // Add to scope
+    $scope.books = books;
+    
 });
