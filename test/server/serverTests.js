c0b0d256bc3a62570c3bd499a6746c12b01da908
Olivia Ifrim <olly2204@yahoo.com>
2016-02-21 02:23:31 +0100

    Adding goodreads api - get shelves

--- test/server/serverTests.js
+++ test/server/serverTests.js
@@ -3,6 +3,7 @@ var should = require("should");
 
 // Dev dependencies
 var app = require("./../../app");
+var goodreadsApi = require("./../../lib/goodreadsApi");
 
 // Server tests
 describe("server tests", function () {
@@ -10,5 +11,28 @@ describe("server tests", function () {
     it("should be able to create server", function (done) {
         should.exist(app);
         done();
+    });
+
+    describe("goodreads api", function () {
+
+        it("should be able to get shelves", function (done) {
+
+            goodreadsApi.getShelves("5391468", function (results) {
+                should.exist(results);
+
+                results.should.be.array;
+                results.length.should.eql(5);
+
+                var properties = ["id", "name", "book_count"];
+
+                results.forEach(function (result) {
+                    properties.forEach(function (property) {
+                        result.should.have.property(property);
+                    }, this);
+                }, this);
+
+                done();
+            })
+        });
     });
 });
